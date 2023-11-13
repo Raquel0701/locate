88768337a9f09136fcc290d47a8701100bc3581f
Raquel0701 <84461281+Raquel0701@users.noreply.github.com>
2023-11-13 00:55:45 -0500

    Update GoogleMaps.js

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react';
 const GoogleMaps = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDIixEC8HeZzh0VF0ANfGHpg309kDO-x0A&callback=initMap&libraries=places&v=weekly`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDIixEC8HeZzh0VF0ANf9kDO-x0A&callback=initMap&libraries=places&v=weekly`;
     script.defer = true;
     document.head.appendChild(script);
 
